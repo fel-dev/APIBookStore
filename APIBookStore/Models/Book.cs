d8aa76254e91e904b11a61bfa5550f6f6255453d
Felipe Carneiro <correaflp@gmail.com>
2024-03-22 14:41:02 -0300

    Criando uma API com C# e MongoDB 1/5

--- APIBookStore/Models/Book.cs
+++ APIBookStore/Models/Book.cs
@@ -1,6 +1,8 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
+// This class is used to represent the Book model in the application. It contains the properties of the book such as Id, BookName, Price, Category, and Author. The Id property is decorated with the BsonId attribute to specify that it is the primary key of the document. The BsonRepresentation attribute is used to specify the representation of the Id property in the database. The BookName, Category, and Author properties are decorated with the BsonElement attribute to specify the name of the properties in the database. The Price property is used to store the price of the book.
+
 namespace APIBookStore.Models
 {
     public class Book
